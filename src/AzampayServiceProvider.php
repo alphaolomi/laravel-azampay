9dcbb28a1c91d411b456f108d24ec29c0401330e
Alpha Olomi <alphaolomi@gmail.com>
2023-07-22 03:11:34 +0300

    Update AzampayServiceProvider.php

--- src/AzampayServiceProvider.php
+++ src/AzampayServiceProvider.php
@@ -23,6 +23,30 @@ class AzampayServiceProvider extends PackageServiceProvider
     public function packageRegistered()
     {
         $this->app->bind('azampay', function () {
+            $data = config('azampay');
+            if (empty($data)) {
+                throw new \Exception('Azampay config not found');
+            }
+
+            if($data['appName']) {
+                throw new \Exception('AppName is required');
+            }
+
+            if($data['clientId']) {
+                throw new \Exception('ClientId is required');
+            }
+
+            if($data['clientSecret']) {
+                throw new \Exception('ClientSecret is required');
+            }
+
+            // if($data['environment']) {
+            //     throw new \Exception('Environment is required');
+            // }
+
+            // if($data['token']) {
+            //     throw new \Exception('Token is required');
+            // }
             return new AzampayService();
         });
     }
